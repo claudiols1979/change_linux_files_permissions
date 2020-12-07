4be62003d2370087dbf8ca60cc5eb4a7c2f5b6dc
claudiols1979 <claudiols1979@hotmail.com>
2020-12-07 17:32:46 -0600

    trying to implement recursive change permissions

--- change_files_permissions.py
+++ change_files_permissions.py
@@ -21,13 +21,27 @@ def make_list(path):
                 my_list.append(full_path)                
     return my_list
 
-# def make_list_all(path):
+# def make_list_recursive(path):
+#     #create list of file out of directory using its path
 #     my_list = []
-#     for f in listdir(path):
-#         full_path = path+f                           
-#         if f not in my_list:                
-#             my_list.append(full_path)
-#             print(my_list)                
+#     for root, dirs, files in os.walk(path):               
+#         #full_path = root+'/'+dirs+'/'+files   
+#         #print(full_path)    
+#         for name in files:
+#             print(os.path.join(root, name))
+#             if isfile(join(root, name)):                        
+#                 if name not in my_list:
+#                     full_path1 = root+'/'+name
+#                     print(full_path1)                
+#                     my_list.append(full_path1)
+#         for name in dirs:
+#             print(os.path.join(root, name))
+#             #base = os.path.split(full_path)[0]
+#             if isfile(join(root, name)):                        
+#                 if name not in my_list:
+#                     full_path2 = root+'/'+name
+#                     print(full_path2)                
+#                     my_list.append(full_path2)                
 #     return my_list
 
 def make_exec(list, perm_octal):
@@ -50,7 +64,7 @@ def perm(list):
 
 def main():
     # if sys.argv[3] == True:
-    #     new_list = make_list_all(path)        
+    #     new_list = make_list_recursive(path)        
     #     make_exec(new_list, perm_octal)
     #     perm(new_list)
     # else:
@@ -60,13 +74,26 @@ def main():
 
 if __name__ == "__main__":    
     import argparse
+
+    def str2bool(v):
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+
     #create command line arguments. "path" and "perm_octal" which is the permission in octal form
     parser = argparse.ArgumentParser(description='Change files & directory permissions')
     parser.add_argument("path", help="Path where files are found. Make sure to type full directory path as /home/user/folder/ note last slash '/'", type=str)    
     parser.add_argument('perm_octal', help="Octal permissions", type=int)
-    #parser.add_argument('all', help="Change permission of directories as well. Possible values True or False", type=bool)
+    # parser.add_argument('all', type=str2bool, nargs='?',  
+    #                         const=True, default=False,
+    #                         help="Change permission recursively. Possible values True or False",)
     args = parser.parse_args()
     path = args.path
     perm_octal = args.perm_octal
-    #all = args.all   
+    # all_ = args.all
     main()
--- dev_tests/test_functions.py
+++ dev_tests/test_functions.py
@@ -2,21 +2,31 @@
 import os
 from os import listdir, path, walk
 from os.path import isfile, join
+from os import walk
 
 
-def make_list(path):
+def make_list_recursive(path):
     #create list of file out of directory using its path
     my_list = []
-    for f in listdir(path):
-        print(f)        
-        full_path = path+'/'+f    
-        print(full_path)    
-        base = os.path.split(full_path)[0]
-        if isfile(join(base, f)):                        
-            if f not in my_list:
-                print(f)                
-                my_list.append(full_path)                
+    for root, dirs, files in os.walk(path):               
+        #full_path = root+'/'+dirs+'/'+files   
+        #print(full_path)    
+        for name in files:
+            print(os.path.join(root, name))
+            if isfile(join(root, name)):                        
+                if name not in my_list:
+                    full_path1 = root+'/'+name
+                    print(full_path1)                
+                    my_list.append(full_path1)
+        for name in dirs:
+            print(os.path.join(root, name))
+            #base = os.path.split(full_path)[0]
+            if isfile(join(root, name)):                        
+                if name not in my_list:
+                    full_path2 = root+'/'+name
+                    print(full_path2)                
+                    my_list.append(full_path2)                
     return my_list
 
-new_list = make_list('/home/claudio/change_files_permissions')
+new_list = make_list_recursive('/home/claudio/node')
 print(new_list)
